aae7021e06897e5fde96179eeed90efcb07d4063
Ramya <ramyacp132@gmail.com>
2020-04-21 23:47:49 +0530

    Removed the Duplicate folder

--- myvenv/news_aggregator/forms.py
+++
@@ -1,6 +0,0 @@
-from django import forms
-from .models import Article
-class Post_article(forms.ModelForm):
-	class Meta:
-		model=Article
-		fields=('title','image_url','blog_url')
--- myvenv/news_aggregator/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class Article(models.Model):
-    title=models.CharField(max_length=400)
-    image_url=models.URLField(max_length=500)
-    blog_url=models.URLField(max_length=600)
-    def __str__(self):
-        return self.title
