5b11013a896bd55e7ae33839abc1f359899618f2
Ramya <48626414+RAMYA-CP@users.noreply.github.com>
2020-08-09 21:40:15 +0530

    Updated views with News api

--- myvenv/backend/News_aggregator/views.py
+++ myvenv/backend/News_aggregator/views.py
@@ -1,89 +1,131 @@
 from django.shortcuts import render,redirect
 from .models import Articles
 import requests
+import json
 from bs4 import BeautifulSoup
 from collections import OrderedDict
 from .forms import Post_article
 def homepage(request):
-    urls=requests.get("https://www.bbc.com/news")
-    soup=BeautifulSoup(urls.text,'html.parser')
-    t=soup.find("div",{'class':'gel-layout gel-layout--no-flex nw-c-top-stories--standard nw-c-top-stories--international'})
-    # t.find('a',{'class':'nw-c-feature-promo nw-o-link-split__anchor nw-o-link--white gs-u-display-block'}).decompose()
-    f=t.select('h3')
+    url = "http://newsapi.org/v2/everything?from=2020-07-19&q=general news&sortBy=popularity&apiKey=8bcdf6bc1bac4a8aa449ff96577b2480"
+    response = requests.request("GET", url)
+    df=json.loads(response.text)
     content=[]
-    for l in f[2:]:
-        content.append(l.text)
-    content=list(OrderedDict.fromkeys(content))
-    an=t.find_all('a',{'class':'gs-c-promo-heading gs-o-faux-block-link__overlay-link gel-pica-bold nw-o-link-split__anchor'})
     links=[]
-    for ik in an:
-        link=ik.get('href')
-        if "https://www.bbc.co.uk" not in link:
-            link="https://www.bbc.co.uk"+link
-        links.append(link)
-    links=list(OrderedDict.fromkeys(links))
-    im=t.select('img')
     images=[]
-    for i in im[1:]:
-        tu=i.get('data-src')
-        tu=str(tu)
-        tu=tu.replace('{width}','800')
-        images.append(tu)
+    for i in range(0,15):
+        contents=dict(df['articles'][i])
+        content.append(contents['title'])
+        links.append(contents['url'])
+        images.append(contents['urlToImage'])
+        # top_news.append([contents['title'],contents['url'],contents['urlToImage']])
+    # urls=requests.get("https://www.bbc.com/news")
+    # soup=BeautifulSoup(urls.text,'html.parser')
+    # t=soup.find("div",{'class':'gel-layout gel-layout--no-flex nw-c-top-stories--standard nw-c-top-stories--international'})
+    # # t.find('a',{'class':'nw-c-feature-promo nw-o-link-split__anchor nw-o-link--white gs-u-display-block'}).decompose()
+    # f=t.select('h3')
+    # content=[]
+    # for l in f[2:]:
+    #     content.append(l.text)
+    content=list(OrderedDict.fromkeys(content))
+    # an=t.find_all('a',{'class':'gs-c-promo-heading gs-o-faux-block-link__overlay-link gel-pica-bold nw-o-link-split__anchor'})
+    # links=[]
+    # for ik in an:
+    #     link=ik.get('href')
+    #     if "https://www.bbc.co.uk" not in link:
+    #         link="https://www.bbc.co.uk"+link
+    #     links.append(link)
+    links=list(OrderedDict.fromkeys(links))
+    # im=t.select('img')
+    # images=[]
+    # for i in im[1:]:
+    #     tu=i.get('data-src')
+    #     tu=str(tu)
+    #     tu=tu.replace('{width}','800')
+    #     images.append(tu)
     images=list(OrderedDict.fromkeys(images))
     conima=tuple(zip(links,content,images))
-    url=requests.get('https://abcnews.go.com/International')
-    soup=BeautifulSoup(url.text,'html.parser')
-    t=soup.find_all('div',class_='band__lead')
-    contents=[]
+    # url=requests.get('https://abcnews.go.com/International')
+    # soup=BeautifulSoup(url.text,'html.parser')
+    # t=soup.find_all('div',class_='band__lead')
+    # contents=[]
+    # titles=[]
+    # for i in t:
+    #     content=i.find_all('section')
+    #     for j in content[0:1]:
+    #         pp=j.select('a')
+    #         for kk in pp:
+    #             contents.append(kk.get('href'))
+    #             titles.append(kk.text)
+    url = "http://newsapi.org/v2/everything?from=2020-07-19&q=top news&sortBy=popularity&apiKey=8bcdf6bc1bac4a8aa449ff96577b2480"
+    response = requests.request("GET", url)
+    df=json.loads(response.text)
+    links=[]
     titles=[]
-    for i in t:
-        content=i.find_all('section')
-        for j in content[0:1]:
-            pp=j.select('a')
-            for kk in pp:
-                contents.append(kk.get('href'))
-                titles.append(kk.text)
-    articles=tuple(zip(contents,titles))
+    for i in range(0,15):
+        contents=dict(df['articles'][i])
+        titles.append(contents['title'])
+        links.append(contents['url'])
+    articles=tuple(zip(links,titles))
 
     db=Articles.objects.all()
-
-    urls=requests.get("https://edition.cnn.com/entertainment")
-    soup=BeautifulSoup(urls.text,'html.parser')
-    r=soup.find_all('div',{'class':'zn__containers'})
+    url = "http://newsapi.org/v2/everything?from=2020-07-19&q=entertainment&sortBy=popularity&apiKey=8bcdf6bc1bac4a8aa449ff96577b2480"
+    response = requests.request("GET", url)
+    df=json.loads(response.text)
     entlinks=[]
-    entimages=[]
     entitles=[]
-    for i in r:
-        se=i.find_all('li',{'class':'cn__listitem'})
-        for j in se:
-            it=j.find('div',{'class','media'})
-            it=it.find('a')
-            entlinks.append('https://edition.cnn.com/'+it.get('href')[1:])
-            it=it.find('img')
-            entimages.append('https:'+it.get('data-src-medium'))
-            it=j.find('span',{'class','cd__headline-text'})
-            entitles.append(it.text)
+    entimages=[]
+    for i in range(0,15):
+        contents=dict(df['articles'][i])
+        entitles.append(contents['title'])
+        entlinks.append(contents['url'])
+        entimages.append(contents['urlToImage'])
+    # urls=requests.get("https://edition.cnn.com/entertainment")
+    # soup=BeautifulSoup(urls.text,'html.parser')
+    # r=soup.find_all('div',{'class':'zn__containers'})
+    # entlinks=[]
+    # entimages=[]
+    # entitles=[]
+    # for i in r:
+    #     se=i.find_all('li',{'class':'cn__listitem'})
+    #     for j in se:
+    #         it=j.find('div',{'class','media'})
+    #         it=it.find('a')
+    #         entlinks.append('https://edition.cnn.com/'+it.get('href')[1:])
+    #         it=it.find('img')
+    #         entimages.append('https:'+it.get('data-src-medium'))
+    #         it=j.find('span',{'class','cd__headline-text'})
+    #         entitles.append(it.text)
     content_ent=zip(entlinks,entimages,entitles)
-
-    urls=requests.get("https://www.firstpost.com/category/sports")
-    soup=BeautifulSoup(urls.text,'html.parser')
-    r=soup.find_all('ul',{'class':'list side-img two-column'})
+    url = "http://newsapi.org/v2/everything?from=2020-07-19&q=sports&sortBy=popularity&apiKey=8bcdf6bc1bac4a8aa449ff96577b2480"
+    response = requests.request("GET", url)
+    df=json.loads(response.text)
     splinks=[]
-    spimages=[]
     sptitles=[]
-    for i in r:
-        se=i.find_all('li')
-        for j in se:
-            it=j.find('a')
-            splinks.append(it.get('href'))
-            it=it.find('img')
-            image='https:'+it.get('src')
-            image=image.replace('142','300')
-            image=image.replace('106','300')
-            spimages.append(image)
-            it=j.find('a')
-            it=it.find('h3')
-            sptitles.append(it.text.strip())
+    spimages=[]
+    for i in range(0,15):
+        contents=dict(df['articles'][i])
+        sptitles.append(contents['title'])
+        splinks.append(contents['url'])
+        spimages.append(contents['urlToImage'])
+    # urls=requests.get("https://www.firstpost.com/category/sports")
+    # soup=BeautifulSoup(urls.text,'html.parser')
+    # r=soup.find_all('ul',{'class':'list side-img two-column'})
+    # splinks=[]
+    # spimages=[]
+    # sptitles=[]
+    # for i in r:
+    #     se=i.find_all('li')
+    #     for j in se:
+    #         it=j.find('a')
+    #         splinks.append(it.get('href'))
+    #         it=it.find('img')
+    #         image='https:'+it.get('src')
+    #         image=image.replace('142','300')
+    #         image=image.replace('106','300')
+    #         spimages.append(image)
+    #         it=j.find('a')
+    #         it=it.find('h3')
+    #         sptitles.append(it.text.strip())
     content_spt=zip(splinks,spimages,sptitles)
     
     contexts={
@@ -109,21 +151,10 @@ def post_article(request):
 def search(request):
     if request.method == 'GET': # If the form is submitted
         search_query = request.GET.get('q')
-        # links_list=[]
-        # urls=requests.get("https://www.google.com/search?q={0}&source=lnms&tbm=nws".format(search_query))
-        # soup=BeautifulSoup(urls.text,'html.parser')
-        # r=soup.select('.kCrYT a')
-        # print("Related links: ")
-        # for l in r[1:7]:
-        #     link=l.get('href')
-        #     if(link.find('https://www.google.com')==-1):
-        #         links_list.append('http://www.google.com'+link)
-        # print(links_list) \
         art_url=[]
         urls=requests.get("https://www.google.com/search?q={0}".format(search_query))
         soup=BeautifulSoup(urls.text,'html.parser')
         r=soup.select('.kCrYT a')
-    #     print(r)
         print("Related links: ")
         title_list=[]
         for l in r[1:7]:
